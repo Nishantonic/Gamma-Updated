94ad31d1305d62364508d87733b283901b901a2c
Prasad Sunil Dinnapurkar <122858378+Dinnapurkarprasad@users.noreply.github.com>
2025-01-15 20:05:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Page from "./components/docView/page"
 import CardTemplates from "./components/docView/slidesView/CardTemplates"
+import GenerateAi from "./components/docView/GenerateAi"
 
 function App() {
 
   return (
-    <>
-      <Page/>
-      {/* <CardTemplates/> */}
-    </>
+   <Router>
+       <Routes>
+          <Route path="/" element={<Page/>}/>
+          <Route path="/generate-ai" element={<GenerateAi />} />
+       </Routes>
+   </Router>
   )
 }
 
 export default App
+
