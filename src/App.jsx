7195c7362706c82a0abdb2be27dd818bedbfdab6
BhautikNimbark <bhautiknimbark245@gmail.com>
2025-03-20 23:58:30 +0530

    changes done as clinet say for api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Page from "./components/docView/page"
+import Page from "./components/docView/page";
 import SignUp from "./components/docView/SignUp";
 import Login from "./components/docView/Login";
 import Dashboard from "./components/dashboard/Dashboard";
@@ -7,26 +7,28 @@ import { PresentationMode } from "./components/docView/PresentationMode";
 import GenerateAiPage from "./components/docView/GenerateAiPage";
 import { DroppedItemsProvider } from "./components/docView/DroppedItemsContext";
 import SharePage from "./components/docView/SharePage";
-function App() {
 
+function App() {
   return (
-   <Router>
-       <Routes>
-       <Route path="/page" element={
+    <Router>
+      <Routes>
+        <Route
+          path="/page"
+          element={
             <DroppedItemsProvider>
-              <Page/>
-            </DroppedItemsProvider>}/>
-          <Route path="/generate-ai" element={<GenerateAiPage />} />
-          <Route path="/" element={<SignUp/>}/>
-          <Route path="/home" element={<Dashboard/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/share/:shareId" element={<SharePage />} />
-          <Route path="/presentation" element={<PresentationMode/>}/>
-       </Routes>
-   </Router>
-  )
+              <Page />
+            </DroppedItemsProvider>
+          }
+        />
+        <Route path="/generate-ai" element={<GenerateAiPage />} />
+        <Route path="*" element={<Login />} />
+        <Route path="/home" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/share/:shareId" element={<SharePage />} />
+        <Route path="/presentation" element={<PresentationMode />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
-
-
+export default App;
